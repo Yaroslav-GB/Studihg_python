a29c11c2eee126373434a922a85b60681c4c0f82
Yaroslav <77globex@mail.ru>
2021-02-08 17:00:53 +0300

    Задача №5 урока №6

--- lesson_6/task_6_5.py
+++ lesson_6/task_6_5.py
@@ -12,4 +12,27 @@
 """
 
 class Stationery:
-    title =
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'{self}'
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+
+pencil = Stationery('pencil')
+pencil.title = 'Карандашем'
+
+pen = Stationery('pen')
+pen.title = 'Ручкой'
+
+handle = Stationery('handle')
+handle.title = 'Маркером'
+
+print(f'Запуск отрисовки {pencil.draw()}')
+print(f'Запуск отрисовки {pen.draw()}')
+print(f'Запуск отрисовки {handle.draw()}')
+
